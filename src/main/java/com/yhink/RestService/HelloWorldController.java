a35fc98af2ab4070c728ba40f2e29b50e24bcd1f
Newton Bii <32613818+NewtonBii@users.noreply.github.com>
2022-02-25 21:08:44 +0300

    Feature today

--- src/main/java/com/yhink/RestService/HelloWorldController.java
+++ src/main/java/com/yhink/RestService/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/")
     public String hello() {
-        return "Hello World Wohoo.";
+        return "Hello World Today";
     }
 }
